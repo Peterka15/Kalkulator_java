fc72333bb4f570be610c0930c9999353e12787ec
Peterka <Peterka@DESKTOP-Q6KSP34.Dlink>
2020-10-14 01:24:36 +0200

    Pierwszy commit w Javie

---
+++ kredyt_01/src/com/jsfcourse/calc/KredytBB.java
@@ -0,0 +1,79 @@
+package com.jsfcourse.calc;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+@Named
+@RequestScoped
+//@SessionScoped
+public class KredytBB {
+	private String kwota;
+	private String oprocentowanie;
+	private Double rata;
+
+//	7000 - 100%
+//	315	 - 4,5%
+	
+	public String getKwota() {
+		return kwota;
+	}
+
+	public void setKwota(String kwota) {
+		this.kwota = kwota;
+	}
+
+	public String getOprocentowanie() {
+		return oprocentowanie;
+	}
+
+	public void setOprocentowanie(String oprocentowanie) {
+		this.oprocentowanie = oprocentowanie;
+	}
+
+	
+	public Double getRata() {
+		return rata;
+	}
+
+	public void setRata(Double rata) {
+		this.rata = rata;
+	}
+
+	
+	
+	public boolean doTheMath() {
+		try {
+			double kwota = Double.parseDouble(this.kwota);
+			double oprocentowanie = Double.parseDouble(this.oprocentowanie);
+
+			rata =( (oprocentowanie * kwota)/ 100) ;
+
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
+			return true;
+		} catch (Exception e) {
+			ctx.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "B³¹d podczas przetwarzania parametrów", null));
+			return false;
+		}
+	}
+
+
+
+	@Inject
+	FacesContext ctx;
+
+	// Go to "showresult" if ok
+	public String calc() {
+		
+		if (doTheMath()) {
+			return "showresult";
+		}
+		return null;
+		
+	}
+
+}
